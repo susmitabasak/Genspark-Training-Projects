0b50da5b2b4f03a94651da079393238f14b02568
Susmita Basak <saha.susmita@gmail.com>
2022-02-11 11:10:38 -0700

    added try catch

--- projects/Project1/src/main/java/DragonCave.java
+++ projects/Project1/src/main/java/DragonCave.java
@@ -9,17 +9,24 @@ public class DragonCave {
                 "\nand will share his treasure with you. The other dragon is greedy and hungry and will eat you on sight." +
                 "\nWhich cave will you go into? (1 or 2)");
         Scanner in = new Scanner(System.in);
-        String choice = in.nextLine();
-        if(choice.equals("1"))
-            System.out.println("You approach the cave... It is dark and spooky.. " +
-                    "\nA large dragon jumps in front of you! He opens his jaw and..." +
-                    "\nGobbles you down in one bite!");
-        else if(choice.equals("2"))
-            System.out.println("You approach the cave... It is quite in there... " +
-                    "\nA cute friendly dragon approaches you! " +
-                    "\nHe shares HIS treasure with YOU :)!");
-        else
-            System.out.println("You have made an invalid choice");
+        try {
+            int choice = in.nextInt();
+            if (choice == 1)
+                System.out.println("You approach the cave... It is dark and spooky.. " +
+                        "\nA large dragon jumps in front of you! He opens his jaw and..." +
+                        "\nGobbles you down in one bite!");
+            else if (choice == 2)
+                System.out.println("You approach the cave... It is quite in there... " +
+                        "\nA cute friendly dragon approaches you! " +
+                        "\nHe shares HIS treasure with YOU :)!");
+            else
+                System.out.println("You have made an invalid choice");
+        } catch (NumberFormatException ne) {
+            System.out.println("You have not entered a valid number");
+        } catch (Exception e) {
+            System.out.println("Error " + e.getMessage());
+            e.printStackTrace();
+        }
 
     }
 }
--- projects/Project2/src/main/java/GuessTheNumber.java
+++ projects/Project2/src/main/java/GuessTheNumber.java
@@ -6,38 +6,43 @@ import java.util.Scanner;
 public class GuessTheNumber {
     public static void main(String[] args) {
         System.out.println("Hello! What is your name?");
-        Scanner in = new Scanner(System.in);
-        String name = in.next();
-        Random random = new Random();
-        boolean guessed = false;
-        boolean playAgain  = true;
-        while(playAgain) {
-            System.out.println("Well, " + name + ", I am thinking of a number between 1 and 20.");
-            int secretNumber = random.nextInt(20) + 1;
-            // comment line below when playing actually, used for knowing the secret number for verifying
-            System.out.println("SECRET NUMBER is " +secretNumber);
-            for(int i=0; i<6; i++) {
-                guessed = false;
-                System.out.println("Take a guess");
-                int guess = in.nextInt();
-                if (guess < secretNumber)
-                    System.out.println("Your guess is too low.");
-                else if (guess > secretNumber)
-                    System.out.println("Your guess is too high.");
-                else if (guess == secretNumber) {
-                    System.out.println("Good job, " + name + "! You guessed my number in " + (i+1) + " guesses!");
-                    guessed = true;
-                    break;
+        try {
+            Scanner in = new Scanner(System.in);
+            String name = in.next();
+            Random random = new Random();
+            boolean guessed = false;
+            boolean playAgain = true;
+            while (playAgain) {
+                System.out.println("Well, " + name + ", I am thinking of a number between 1 and 20.");
+                int secretNumber = random.nextInt(20) + 1;
+                // comment line below when playing actually, used for knowing the secret number for verifying
+                System.out.println("SECRET NUMBER is " + secretNumber);
+                for (int i = 0; i < 6; i++) {
+                    guessed = false;
+                    System.out.println("Take a guess");
+                    int guess = in.nextInt();
+                    if (guess < secretNumber)
+                        System.out.println("Your guess is too low.");
+                    else if (guess > secretNumber)
+                        System.out.println("Your guess is too high.");
+                    else if (guess == secretNumber) {
+                        System.out.println("Good job, " + name + "! You guessed my number in " + (i + 1) + " guesses!");
+                        guessed = true;
+                        break;
+                    }
                 }
+                if (!guessed)
+                    System.out.println("Sorry, you have exhausted your 6 tries this time...");
+                System.out.println("Would you like to play again? (y/n)");
+                String play = in.next();
+                if (play.equals("y"))
+                    playAgain = true;
+                else if (play.equals("n"))
+                    playAgain = false;
             }
-            if(!guessed)
-                System.out.println("Sorry, you have exhausted your 6 tries this time...");
-            System.out.println("Would you like to play again? (y/n)");
-            String play = in.next();
-            if(play.equals("y"))
-                playAgain = true;
-            else if(play.equals("n"))
-                playAgain = false;
+        } catch (Exception e) {
+            System.out.println("Exception " + e.getMessage());
+            e.printStackTrace();
         }
     }
 }
